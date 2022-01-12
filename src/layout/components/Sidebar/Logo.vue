39bdf00ed106ab144fd2c43085d6633ff3f68ed1
F.Bai <fangliangbai@gmail.com>
2022-01-12 21:54:25 +0000

    对界面进行了修改以适配项目

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -24,8 +24,8 @@ export default {
   },
   data() {
     return {
-      title: 'Vue Element Admin',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      title: '小微智能洗车 管理平台',
+      logo: 'title-logo.png'
     }
   }
 }
